fa38f497e34d79784554a86bc7b296af332df826
itzme-challa <challawaheed4@gmail.com>
2025-05-25 18:53:02 +0530

    Update order.ts

--- pages/api/cashfree/order.ts
+++ pages/api/cashfree/order.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
@@ -13,13 +13,18 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 
   try {
-    const response = await axios.get(`https://api.cashfree.com/pg/orders/${orderId}`, {
-      headers: {
-        'x-api-version': '2022-09-01',
-        'x-client-id': process.env.CASHFREE_CLIENT_ID,
-        'x-client-secret': process.env.CASHFREE_CLIENT_SECRET,
-      },
-    });
+    const response = await axios.get(
+      process.env.NODE_ENV === 'production'
+        ? `https://api.cashfree.com/pg/orders/${orderId}`
+        : `https://sandbox.cashfree.com/pg/orders/${orderId}`,
+      {
+        headers: {
+          'x-api-version': '2022-09-01',
+          'x-client-id': process.env.CASHFREE_CLIENT_ID,
+          'x-client-secret': process.env.CASHFREE_CLIENT_SECRET,
+        },
+      }
+    );
 
     return res.status(200).json({
       success: true,
